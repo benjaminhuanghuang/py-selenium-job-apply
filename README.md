88edf1b609b2e46f96ab8976aef24374875e96a1
benjaminhuanghuang <bhuang@rms.com>
2021-03-10 12:07:04 -0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Automatically apply for jobs on LinkedIn (Python + Selenium) (2020)
+- https://www.youtube.com/watch?v=6plKL95a134&ab_channel=AICoding
+- https://github.com/coding-ai/EasyApply-Linkedin
+
+RPA robotic process automation
+
+
+## Setup
+
+```
+  python3 -m venv venv3
+
+  pip install selenium
+```
+
+Selenium requires a driver to interactive with the chosen browser. 
+https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+
---
+++ login.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import re
+import json
+
+
+class EasyApplyLinkedin:
+    def __init__(self, data):
+        """Parameter initialization"""
+
+        self.email = data['email']
+        self.password = data['password']
+        self.keywords = data['keywords']
+        self.location = data['location']
+        self.driver = webdriver.Chrome(data['driver_path'])
+
+    def login_linkedin(self):
+        """This function logs into your personal LinkedIn profile"""
+
+        # go to the LinkedIn login url
+        self.driver.get("https://www.linkedin.com/login")
+
+        # introduce email and password and hit enter
+        login_email = self.driver.find_element_by_name('session_key')
+        login_email.clear()
+        login_email.send_keys(self.email)
+        login_pass = self.driver.find_element_by_name('session_password')
+        login_pass.clear()
+        login_pass.send_keys(self.password)
+        login_pass.send_keys(Keys.RETURN)
+
+
+if __name__ == '__main__':
+
+    with open('config.json') as config_file:
+        data = json.load(config_file)
+
+    bot = EasyApplyLinkedin(data)
+    bot.login_linkedin()
